c3bba37ad43ff1bcf4ad6499e3babe07178412ad
niepokon1235 <51741354+niepokon1235@users.noreply.github.com>
2020-04-14 18:46:42 +0200

    Poprawa przesyłu

--- testowe/Src/main.c
+++ testowe/Src/main.c
@@ -113,6 +113,7 @@ int main(void)
   HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
 
+  HAL_TIM_Base_Start(&htim3);
   HAL_UART_Receive_IT(&huart2, &Received, 1);
   /* USER CODE END 2 */
 
